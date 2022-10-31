4246e87628aeea58fe270bc9641170e1dd7990bc
AnaGT <86501883+AnaGT@users.noreply.github.com>
2022-10-30 21:08:25 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -21,7 +21,7 @@ def pregunta_01():
     data = pd.read_csv("data.csv")
 
     # Cree un objeto de tipo `PolynomialFeatures` con grado `2`
-    poly = polynomialFeatures.degree(2)
+    poly = polynomialFeatures.fit(degree=2)
 
     # Transforme la columna `x` del dataset `data` usando el objeto `poly`
     x_poly = poly.transform(data[["x"]])
@@ -48,10 +48,10 @@ def pregunta_02():
         y_pred = np.dot(x_poly, params)
 
         # Calcule el error
-        error = ___ - ___
+        error = y - y_pred
 
         # Calcule el gradiente
-        gradient = ____
+        gradient = -2 * error
 
         # Actualice los parámetros
         params = params - learning_rate * gradient
