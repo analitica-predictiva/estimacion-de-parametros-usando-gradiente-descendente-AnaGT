2ff5c91127363a373154f4605c027c2cb3a7a8ff
AnaGT <86501883+AnaGT@users.noreply.github.com>
2022-11-06 16:03:31 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -46,7 +46,7 @@ def pregunta_02():
     for epoch in range(n_iterations):
 
         # Compute el pronóstico con los parámetros actuales
-        y_pred = np.dot(x_poly, learning_rate)
+        y_pred = np.dot(x_poly, params)
 
         # Calcule el error
         error = [y - y_pred for y, y_pred in zip(y, y_pred)]
