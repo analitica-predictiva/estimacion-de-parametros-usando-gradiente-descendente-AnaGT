e0d67ab3a2253a2e39cad2ab416abd0c1e9446dc
AnaGT <86501883+AnaGT@users.noreply.github.com>
2022-11-06 15:05:20 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -42,7 +42,7 @@ def pregunta_02():
     n_iterations = 1000
 
     # Defina el parámetro inicial `params` como un arreglo de tamaño 3 con ceros
-    params = np.array([x_poly.shape[0,0,0]])
+    params = np.zeros(1 + x_poly.shape[1])
     for epoch in range(n_iterations):
 
         # Compute el pronóstico con los parámetros actuales
